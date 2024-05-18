42b0767c20eef3e10ca0fe32fa7732383713046b
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-05-18 16:14:30 -0300

    first commit

--- src/edu/nicolas/primeiraSemana/MinhaClasse.java
+++ src/edu/nicolas/primeiraSemana/MinhaClasse.java
@@ -3,13 +3,27 @@ package edu.nicolas.primeiraSemana;
 public class MinhaClasse {
     public static void main(String[] args) {
 
+        //propiedades
         String primeiroNome = "Nicolas";
         String segundoNome = "Kenzo";
-        String [] nomes = {"nicolas", "nkenzo"};
+        String[] nomes = {"nicolas", "nkenzo"};
         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        float pi = 3.14F;
+        double salario = 1277.33;
+
+
+        //tipagem do java
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // final diz que está variavel não pode ter seu valor alterado
+        final double VALOR_DE_PI = 3.14;
+
         System.out.println(nomeCompleto);
     }
 
+    //falando sobre métodos
     public static String nomeCompleto(String primeiroNome, String segundoNome) {
         return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }

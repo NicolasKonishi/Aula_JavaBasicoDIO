9aac2ec292f844658e5df95cb1940a905a302963
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-05-18 16:37:40 -0300

    Desafio de Sintaxe - Conta Bancária

---
+++ src/edu/nicolas/desafios/ContaBanco.java
@@ -0,0 +1,27 @@
+package edu.nicolas.desafios;
+
+import java.util.Scanner;
+
+public class ContaBanco {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String Nome = teclado.nextLine();
+
+        System.out.println("\nPor favor, digite a Agência: ");
+        String Agencia = teclado.nextLine();
+
+        System.out.println("\nPor favor, digite o número da Agência: ");
+        int Numero = teclado.nextInt();
+
+        System.out.println("\nPor favor, digite o seu saldo: ");
+        Double Saldo = teclado.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", Nome, Agencia, Numero, Saldo);
+
+
+
+
+    }
+}

84141bfd1fdd629d095a19c258b082a422dbd6b9
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-05-19 13:39:10 -0300

    DesafioContador

---
+++ src/edu/nicolas/desafios/ControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package edu.nicolas.desafios.ControleFluxo;
+
+public class ParametrosInvalidosException extends Exception{
+    ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+}
